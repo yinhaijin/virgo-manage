527c3b436bd8aa4e88c063014e1071ee77e92883
hakim <yinhaijin@hotmail.com>
2018-05-23 15:07:07 +0800

    初始化项目
    初始化项目

---
+++ src/main/java/com/lunzhiyu/virgo/manage/VirgoManageApplication.java
@@ -0,0 +1,18 @@
+package com.lunzhiyu.virgo.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目名称: virgo-manage
+ * 描述:
+ * 作者: hakim
+ * 创建日期: 2018-05-23.
+ * 创建时间: 下午3:02
+ */
+@SpringBootApplication
+public class VirgoManageApplication {
+    public static void main(String[] args){
+        SpringApplication.run(VirgoManageApplication.class, args);
+    }
+}
